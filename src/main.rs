beeabe8924758a74e5dc41924968850415aa4575
Ivan Schuetz <ivanschuetz@gmail.com>
2025-02-06 18:43:19 +0100

    json response

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, web, App, HttpServer, Responder, Result};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct MyObj {
+    name: String,
+}
 
 #[get("/hello/{name}")]
-async fn greet(name: web::Path<String>) -> impl Responder {
-    format!("Hello {name}!")
+async fn greet(name: web::Path<String>) -> Result<impl Responder> {
+    let obj = MyObj {
+        name: name.to_string(),
+    };
+    Ok(web::Json(obj))
 }
 
 #[actix_web::main] // or #[tokio::main]
