7eb29d1c2e4cadc5500dafd004f500c26df1b090
Ivan Schuetz <ivanschuetz@gmail.com>
2025-02-06 19:25:10 +0100

    basic bikes webservice

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,43 @@ use serde::Serialize;
 
 #[derive(Serialize)]
 struct MyObj {
+    id: String,
     name: String,
+    brand: String,
+    price: String,
+    picture: String,
 }
 
-#[get("/hello/{name}")]
-async fn greet(name: web::Path<String>) -> Result<impl Responder> {
-    let obj = MyObj {
-        name: name.to_string(),
-    };
-    Ok(web::Json(obj))
+#[get("/bikes")]
+async fn bikes() -> Result<impl Responder> {
+    Ok(web::Json([
+        MyObj {
+            id: "1".to_string(),
+            name: "My Name 1".to_string(),
+            brand: "Foo Brand".to_string(),
+            price: "999".to_string(),
+            picture: "https://picsum.photos/id/0/200/300".to_string(),
+        },
+        MyObj {
+            id: "2".to_string(),
+            name: "My Name 2".to_string(),
+            brand: "Bar Brand".to_string(),
+            price: "2000".to_string(),
+            picture: "https://picsum.photos/id/1/200/300".to_string(),
+        },
+        MyObj {
+            id: "3".to_string(),
+            name: "My Name 3".to_string(),
+            brand: "Bar Brand".to_string(),
+            price: "580".to_string(),
+            picture: "https://picsum.photos/id/2/200/300".to_string(),
+        },
+    ]))
 }
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(greet))
+    HttpServer::new(|| App::new().service(bikes))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
