03ab6b155489692a252ee8f756658273570d45ea
Ivan Schuetz <ivanschuetz@gmail.com>
2025-02-09 16:45:19 +0100

    several updates
    server ip 0.0.0.0 working to connect iphone

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,13 @@ struct MyObj {
     name: String,
     brand: String,
     price: String,
+    price_number: f32,
     pictures: Vec<String>,
     vendor_link: String,
     electric: bool,
     #[serde(rename = "type")]
     type_: BikeType,
+    descr: String,
 }
 
 #[derive(Serialize)]
@@ -30,6 +32,7 @@ async fn bikes() -> Result<impl Responder> {
             name: "My Name 1".to_string(),
             brand: "Foo Brand".to_string(),
             price: "999".to_string(),
+            price_number: 999.,
             pictures: vec![
                 "https://picsum.photos/id/0/500/700".to_string(),
                 "https://picsum.photos/id/1/500/700".to_string(),
@@ -37,12 +40,14 @@ async fn bikes() -> Result<impl Responder> {
             vendor_link: "https://google.com".to_string(),
             electric: false,
             type_: BikeType::Mountain,
+            descr: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum".to_string()
         },
         MyObj {
             id: "2".to_string(),
             name: "My Name 2".to_string(),
             brand: "Bar Brand".to_string(),
             price: "2000".to_string(),
+            price_number: 2000.,
             pictures: vec![
                 "https://picsum.photos/id/2/500/700".to_string(),
                 "https://picsum.photos/id/3/500/700".to_string(),
@@ -50,12 +55,14 @@ async fn bikes() -> Result<impl Responder> {
             vendor_link: "https://google.com".to_string(),
             electric: false,
             type_: BikeType::Hybrid,
+            descr: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum".to_string()
         },
         MyObj {
             id: "3".to_string(),
             name: "My Name 3".to_string(),
             brand: "Bar Brand".to_string(),
             price: "580".to_string(),
+            price_number: 580.,
             pictures: vec![
                 "https://picsum.photos/id/4/500/700".to_string(),
                 "https://picsum.photos/id/5/500/700".to_string(),
@@ -63,6 +70,7 @@ async fn bikes() -> Result<impl Responder> {
             vendor_link: "https://google.com".to_string(),
             electric: true,
             type_: BikeType::Road,
+            descr: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum".to_string()
         },
     ]))
 }
@@ -70,7 +78,8 @@ async fn bikes() -> Result<impl Responder> {
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(bikes))
-        .bind(("127.0.0.1", 8080))?
+        // .bind(("127.0.0.1", 8080))?
+        .bind(("0.0.0.0", 8080))?
         .run()
         .await
 }
