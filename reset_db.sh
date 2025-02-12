bd04d9781a1335d4a5f35ca8b35e7c6a8024fbe2
ivnsch <ivanschuetz@gmail.com>
2025-02-12 14:49:09 +0100

    comment populate dummy entries

--- reset_db.sh
+++ reset_db.sh
@@ -1,2 +1,2 @@
 psql -h 127.0.0.1 -p 5433 -U tester -d bikematch -f ./init_db.sql
-psql -h 127.0.0.1 -p 5433 -U tester -d bikematch -f ./populate_db.sql
+# psql -h 127.0.0.1 -p 5433 -U tester -d bikematch -f ./populate_db.sql
