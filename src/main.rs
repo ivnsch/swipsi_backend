31fed642432fe6a380e6931543efbc234ac2958d
Ivan Schuetz <ivanschuetz@gmail.com>
2025-02-13 06:19:52 +0100

    remove outdated load_bikes fn

--- src/main.rs
+++ src/main.rs
@@ -24,43 +24,10 @@ struct Bike {
     added_timestamp: i64,
 }
 
-#[get("/bikes")]
-async fn bikes(state: Data<AppState>) -> Result<impl Responder> {
-    Ok(web::Json(load_bikes(&state.db).await))
-}
-
 #[get("/items/{last_timestamp}")]
 async fn items(state: Data<AppState>, path: web::Path<i64>) -> Result<impl Responder> {
     let last_timestamp = path.into_inner();
     Ok(web::Json(load_items(&state.db, last_timestamp).await))
-}
-
-async fn load_bikes(pool: &Pool<Postgres>) -> Vec<Bike> {
-    let res: Vec<Bike> = sqlx::query_as(
-        r#"
-SELECT
-    b.id::TEXT AS id,
-    b.name_,
-    b.price,
-    b.price_number,
-    b.vendor_link,
-    b.type_,
-    b.descr,
-    b.added_timestamp,
-    COALESCE(array_agg(bp.url) FILTER (WHERE bp.url IS NOT NULL), ARRAY[]::TEXT[]) AS pictures
-FROM
-    bike b
-LEFT JOIN
-    bike_pic bp ON b.id = bp.bike_id
-GROUP BY
-    b.id, b.name_, b.price, b.price_number, b.vendor_link, b.type_, b.descr, b.added_timestamp;
-"#,
-    )
-    .fetch_all(pool)
-    .await
-    .expect("error2");
-
-    res
 }
 
 async fn load_items(pool: &Pool<Postgres>, after_timestamp: i64) -> Vec<Bike> {
@@ -115,7 +82,6 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .wrap(Logger::default())
             .app_data(Data::new(AppState { db: pool.clone() }))
-            .service(bikes)
             .service(items)
     })
     // .bind(("127.0.0.1", 8080))?
@@ -145,14 +111,7 @@ async fn init_pool() -> Pool<Postgres> {
 
 #[cfg(test)]
 mod test {
-    use crate::{init_pool, load_bikes, load_items};
-
-    #[tokio::test]
-    async fn test_load_bikes() {
-        let pool = init_pool().await;
-        let bikes = load_bikes(&pool).await;
-        println!("bikes: {:?}", bikes);
-    }
+    use crate::{init_pool, load_items};
 
     #[tokio::test]
     async fn test_load_items_after_timestamp() {
